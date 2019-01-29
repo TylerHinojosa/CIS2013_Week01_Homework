53b2213593424b78b879a069bfd351695589b285
TylerHinojosa <46981261+TylerHinojosa@users.noreply.github.com>
2019-01-29 13:35:05 -0600

    Create Homework.cpp

---
+++ Homework.cpp
@@ -0,0 +1,15 @@
+	#include<iostream>
+	using namespace std;
+	
+	int main()
+	{
+			cout << "=======================================================\n";
+			cout << "Tyler Hinojosa\n";
+			cout << "CIS Student, OSU-OKC - Oklahoma City\n";
+			cout << "\n";
+			cout << "		12312 SW 12th		p: (405) 420-6488\n";
+			cout << "		Yukon, OK			e: Tyler.Hinojosa@okstate.edu\n";
+			cout << "							g: github.com/Tylerhinojosa\n";
+			cout << "===========================================================";
+			return 0;
+	}
