7bd773a963c0aa4275e9473113b380e7a8c63b1b
TylerHinojosa <46981261+TylerHinojosa@users.noreply.github.com>
2019-01-29 21:08:11 -0600

    Update Homework.cpp

--- Homework.cpp
+++ Homework.cpp
@@ -3,13 +3,13 @@
 	
 	int main()
 	{
-			cout << "=======================================================\n";
+			cout << "==============================================================\n";
 			cout << "Tyler Hinojosa\n";
 			cout << "CIS Student, OSU-OKC - Oklahoma City\n";
 			cout << "\n";
 			cout << "		12312 SW 12th		p: (405) 420-6488\n";
 			cout << "		Yukon, OK			e: Tyler.Hinojosa@okstate.edu\n";
 			cout << "							g: github.com/Tylerhinojosa\n";
-			cout << "===========================================================";
+			cout << "==============================================================";
 			return 0;
 	}
